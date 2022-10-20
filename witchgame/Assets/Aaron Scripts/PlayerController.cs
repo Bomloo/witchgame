1b8c74b5e9251a81bff487237278a8bcd8ee0b61
Aaron Lin <aarlin123@gmail.com>
2022-10-19 19:26:39 -0700

    start script, fix gitignore

---
+++ witchgame/Assets/Aaron Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    public void move(float move)
+    {
+
+    }
+
+    public void attack()
+    {
+
+    }
+}
